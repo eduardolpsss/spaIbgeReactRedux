5987f6b9754b6fb2dfa213592ce334b798e67947
Eduardo Pontes <eduardo.pontes2801@gmail.com>
2022-12-23 02:22:06 -0300

    feat/MUITableFullImplementation
    View de dados dos municípios feitos em um table dinâmico que a MUI fornece.

---
+++ src/hooks/useEstados.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+export interface Regiao {
+    id: number;
+    sigla: string;
+    nome: string;
+}
+
+export interface IEstado {
+    id: number;
+    sigla: string;
+    nome: string;
+    regiao: Regiao;
+}
+
+export const useEstados = () => {
+    const [estados, setEstados] = useState<IEstado[]>([]);
+
+    useEffect(() => {
+        fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+            .then((response) => response.json())
+            .then((data) => setEstados(data));
+    }, []);
+    
+    return { estados };
+}
